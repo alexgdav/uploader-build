26c7c8c16aa8da8ee36e03c10c31ed6a06de4f91
alexgdav <alexgdavidenko@gmail.com>
2019-11-04 10:34:34 -0500

    Build model and basic CRUD routes for uploads
    1. completed model for `Upload` resource to close #1
    2. completed basic CRUD routes (w/out AWS) for `Upload` resource to
    close #10
    3. Had app use the routes in `server.js`

---
+++ app/models/upload.js
@@ -0,0 +1,34 @@
+const mongoose = require('mogoose')
+require('dotenv').config()
+
+const uploadSchema = new mongoose.Schema({
+  fileName: {
+    type: String,
+    required: true
+  },
+  fileType: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+},
+
+{
+  timestamps: true,
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true }
+})
+
+// virtual property that generates the file URL
+uploadSchema.virtual('fileUrl').get(function () {
+  // generate value
+  const url = 'https://' + process.env.BUCKET_NAME + '.s3.amazonaws.com/' + this.fileName
+  // return value
+  return url
+})
+
+module.exports = mongoose.model('Upload', uploadSchema)
